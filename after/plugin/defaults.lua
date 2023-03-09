017bb6ac8f57a7dd54a590449ca350a78b8f9eb7
Andrew Thomas <andrewmthomas87@gmail.com>
2023-03-09 00:30:20 -0700

    go auto-format, tab width

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,4 @@
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -8,7 +8,7 @@ return {
     local async = event == "BufWritePost"
 
     null_ls.setup({
-      sources = { null_ls.builtins.formatting.prettier },
+      sources = { null_ls.builtins.formatting.prettier, null_ls.builtins.formatting.gofmt, null_ls.builtins.formatting.goimports },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
           vim.keymap.set("n", "<Leader>f", function()
