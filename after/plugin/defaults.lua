1484382e60d54c9216ed47666ef6477c539841aa
Andrew Thomas <andrewmthomas87@gmail.com>
2023-03-26 18:11:31 -0600

    Improve tab behavior

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,7 @@
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
