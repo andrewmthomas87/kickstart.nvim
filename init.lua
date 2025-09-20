6f348f79f6e8a5d9dd39aee2065b0708c333d8b3
Andrew Thomas <andrewmthomas87@gmail.com>
2025-09-20 17:27:44 -0600

    conform java google-java-format

--- init.lua
+++ init.lua
@@ -770,6 +770,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        java = { 'google-java-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
